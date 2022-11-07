4c0053a7f27cc532b365baa62d0dc3e10d32440e
Shobhan Karthick <loganshobhanwolverine@gmail.com>
2022-11-08 01:09:07 +0530

    feat: fixed layout for the website using Navbar and Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React, { Suspense, lazy } from "react"
-import { ChakraProvider, theme, } from "@chakra-ui/react"
+import { ChakraProvider, theme, VStack } from "@chakra-ui/react"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom"
+import {BrowserRouter as Router, Routes, Route, Navigate, } from "react-router-dom"
 
+const Navbar = lazy(() => import("./components/Navbar"))
 const Home = lazy(() => import("./pages/Home"))
 const Project = lazy(() => import("./pages/Project"))
 const ProjectHead = lazy(() => import("./pages/ProjectHead"))
@@ -16,15 +17,18 @@ export const App = () => (
   <ChakraProvider theme={theme}>
     <Router>
       <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<ProjectTable />} />
-          <Route path="/projects/:id" element={<Project />} />
-          <Route path="/projectHeads/:id" element={<ProjectHead />} />
-          <Route path="/application" element={<Application />} />
-          <Route path="/team" element={<Team />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
+        <VStack className="layout" align="stetch">
+          <Navbar />
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/projects" element={<ProjectTable />} />
+              <Route path="/projects/:id" element={<Project />} />
+              <Route path="/projectHeads/:id" element={<ProjectHead />} />
+              <Route path="/application" element={<Application />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </VStack>
       </Suspense>
     </Router>
   </ChakraProvider>
