3ef976a48d191f0bb149eee4ea8f4425c805f829
Shobhan Karthick <loganshobhanwolverine@gmail.com>
2022-11-08 11:39:15 +0530

    style: removed the padding from nav-items in Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,13 +5,13 @@ import { Flex, Text, Spacer, Stack, Button } from '@chakra-ui/react'
 function Navbar() {
   return(
     <Flex>
-      <Text p={4} className="nav-items-logo" as="b" fontSize='3xl'>microbase.</Text>
+      <Text className="nav-items-logo" as="b" fontSize='3xl'>microbase.</Text>
       <Spacer />
-      <Flex direction="row" gap={5} alignItems="center">
-        <Text className="nav-items-menu" p={4} fontSize="18px"><Link to="/">Home</Link></Text>
-        <Text className="nav-items-menu" p={4} fontSize="18px"><Link to="/projects">Projects</Link></Text>
-        <Text className="nav-items-menu" p={4} fontSize="18px"><Link to="/team">Team</Link></Text>
-        <Link className="nav-items-menu" to="/application"><Button m={4} fontSize="18px" backgroundColor="#4ea576" color="#fff" borderRadius={25} padding={"0 20px"}> Apply </Button></Link>
+      <Flex direction="row" gap={10} alignItems="center">
+        <Text className="nav-items-menu" fontSize="18px"><Link to="/">Home</Link></Text>
+        <Text className="nav-items-menu" fontSize="18px"><Link to="/projects">Projects</Link></Text>
+        <Text className="nav-items-menu" fontSize="18px"><Link to="/team">Team</Link></Text>
+        <Link className="nav-items-menu" to="/application"><Button fontSize="18px" backgroundColor="#4ea576" color="#fff" borderRadius={25} padding={"0 20px"}> Apply </Button></Link>
       </Flex>
     </Flex>
   )
