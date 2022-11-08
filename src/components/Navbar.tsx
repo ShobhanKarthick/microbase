725fe7501d8e235e41479e84e22f38440dec3c7c
Shobhan Karthick <loganshobhanwolverine@gmail.com>
2022-11-08 13:17:50 +0530

    feat: user <GreenButton /> tag instead of <Button> tag

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,18 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import { Flex, Text, Spacer, Stack, Button } from '@chakra-ui/react'
+import { Flex, Text, Spacer, Stack, } from '@chakra-ui/react'
+import GreenButton from "../components/GreenButton"
 
 function Navbar() {
   return(
-    <Flex>
+    <Flex p={8}>
       <Text className="nav-items-logo" as="b" fontSize='3xl'>microbase.</Text>
       <Spacer />
       <Flex direction="row" gap={10} alignItems="center">
         <Text className="nav-items-menu" fontSize="18px"><Link to="/">Home</Link></Text>
         <Text className="nav-items-menu" fontSize="18px"><Link to="/projects">Projects</Link></Text>
         <Text className="nav-items-menu" fontSize="18px"><Link to="/team">Team</Link></Text>
-        <Link className="nav-items-menu" to="/application"><Button fontSize="18px" backgroundColor="#4ea576" color="#fff" borderRadius={25} padding={"0 20px"}> Apply </Button></Link>
+        <Link className="nav-items-menu" to="/application"><GreenButton buttonText="Apply" /></Link>
       </Flex>
     </Flex>
   )
