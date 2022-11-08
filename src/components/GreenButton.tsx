654af912f36d8f418944c7171f1ed02171b38fc0
Shobhan Karthick <loganshobhanwolverine@gmail.com>
2022-11-08 17:52:53 +0530

    feat: allowed other props to be passed via <GreenButton />

--- src/components/GreenButton.tsx
+++ src/components/GreenButton.tsx
@@ -1,15 +1,17 @@
 
 import React from "react"
-import { Button, WrapItem } from '@chakra-ui/react'
+import { Button } from '@chakra-ui/react'
 
 interface Props {
   buttonText: string,
   width?: string | number,
+  [propName: string]: any,
 }
 
 function GreenButton(props: Props) {
+  const { buttonText, width, ...rest} = props
   return(
-      <Button fontSize="18px" backgroundColor="#4ea576" color="#fff" borderRadius={25} padding={"0 20px"} w={props.width}> {props.buttonText} </Button>
+      <Button {...rest} fontSize="18px" backgroundColor="#4ea576" color="#fff" borderRadius={25} padding={"0 20px"} w={props.width}> {props.buttonText} </Button>
   )
 }
 
